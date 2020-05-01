8fb7984d2f714fd9c6829eeaf0b378d6ca1a8456
jbblanton <jessicabblanton@gmail.com>
2020-05-01 16:24:22 +0000

    Final, working

--- accounting.py
+++ accounting.py
@@ -16,17 +16,17 @@ def cust_pay_log(txt_file):
     purchase = int(words[2]) * MELON_COST
     paid = float(words[3])
 
-  if purchase != paid:   #Report filters only those who under or over paid
-    print(f"{cust_name} paid ${paid:.2f},",
+    if purchase != paid:   #Report filters only those who under or over paid
+      print(f"{cust_name} paid ${paid:.2f},",
           f"expected ${purchase:.2f}"
           )
     
-  if purchase > paid:
-    print(f"{cust_name} under paid for their melons by {purchase - paid}.")
+      if purchase > paid:
+        print(f"{cust_name} under paid for their melons by ${purchase - paid:.2f}.")
 
-  if purchase < paid:
-    print(f"{cust_name} has overpaid for their melons by {paid - purchase}.")
+      elif purchase < paid:
+        print(f"{cust_name} has overpaid for their melons by ${paid - purchase:.2f}.")
 
   the_file.close()    
 
-cust_pay_log("customer_orders.txt")
+cust_pay_log("customer-orders.txt")
