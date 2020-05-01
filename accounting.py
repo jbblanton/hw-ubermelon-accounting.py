7637546ea64538b2880557459c7446fc7db0e84f
jbblanton <jessicabblanton@gmail.com>
2020-05-01 01:57:52 +0000

    Completed, Untested

--- accounting.py
+++ accounting.py
@@ -1,61 +1,32 @@
-melon_cost = 1.00
 
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
+MELON_COST = 1.00
 
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
+def cust_pay_log(txt_file):
+  """ To pull logs and compare paid v. owed """
 
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
+    
+  the_file = open(txt_file)  #Here we access the txt file and prep the data
+  
+  for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
 
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
+    cust_num = words[0]
+    cust_name = words[1]
+    purchase = int(words[2]) * MELON_COST
+    paid = float(words[3])
 
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
-
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
-
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print(f"{customer1_name} paid ${customer1_paid:.2f},",
-          f"expected ${customer1_expected:.2f}"
+  if purchase != paid:   #Report filters only those who under or over paid
+    print(f"{cust_name} paid ${paid:.2f},",
+          f"expected ${purchase:.2f}"
           )
+    
+  if purchase > paid:
+    print(f"{cust_name} under paid for their melons by {purchase - paid}.")
 
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print(f"{customer2_name} paid ${customer2_paid:.2f},",
-          f"expected ${customer2_expected:.2f}"
-          )
+  if purchase < paid:
+    print(f"{cust_name} has overpaid for their melons by {paid - purchase}.")
 
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print(f"{customer3_name} paid ${customer3_paid:.2f},",
-          f"expected ${customer3_expected:.2f}"
-          )
+  the_file.close()    
 
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print(f"{customer4_name} paid ${customer4_paid:.2f},",
-          f"expected ${customer4_expected:.2f}"
-          )
-
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print(f"{customer5_name} paid ${customer5_paid:.2f},",
-          f"expected ${customer5_expected:.2f}"
-          )
-
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print(f"{customer6_name} paid ${customer6_paid:.2f},",
-          f"expected ${customer6_expected:.2f}"
-          )
+cust_pay_log("customer_orders.txt")
